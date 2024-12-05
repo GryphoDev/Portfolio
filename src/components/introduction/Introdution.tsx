9620f4b6e6be9fd505d9f5b4ed4b5234212340bd
GryphoDev <wmauroux@icloud.com>
2024-12-05 07:15:49 +0100

    feat: replace lorem

--- src/components/introduction/Introdution.tsx
+++ src/components/introduction/Introdution.tsx
@@ -11,10 +11,10 @@ export default function Introdution() {
           Front-End <span>Developer</span>
         </h3>
         <p className="description">
-          Hello, my name is William Mauroux, nice to meet you I would like to
-          welcome you with my personal portfolio. Hello, my name is William
-          Mauroux, nice to meet you I would like to welcome you with my personal
-          portfolio.
+          Welcome to my portfolio. Passionate about web development, I create
+          modern, interactive websites tailored to your needs. My goal is to
+          transform your ideas into unique user experiences. Explore my projects
+          and achievements below!
         </p>
       </div>
       <Image className="profilImage" src={me} alt="William Mauroux" />
--- src/components/technoCard/TechnoCard.tsx
+++ src/components/technoCard/TechnoCard.tsx
@@ -10,25 +10,25 @@ const techno = [
   {
     name: "React",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio vitae architecto quo officia magnam. Cupiditate, harum. Culpa nihil deserunt facilis, corrupti cumque eligendi beatae!",
+      "I use React because it offers great flexibility and makes it easy to build interactive and responsive applications. Its component-based architecture allows me to create modular and scalable interfaces, while its powerful state management system boosts productivity. I particularly appreciate the large React community, which provides access to a wealth of tools and resources.",
     image: react,
   },
   {
     name: "Tailwind",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio vitae architecto quo officia magnam. Cupiditate, harum. Culpa nihil deserunt facilis, corrupti cumque eligendi beatae!",
+      "Tailwind CSS has become one of my favorite tools because it allows me to quickly design sleek, responsive interfaces without having to write complex CSS. With its utility-first approach, I can customize every aspect of the design directly in the HTML, making development faster and more efficient. Its simplicity and flexibility make it an excellent choice for projects where speed and quality are key.",
     image: tailwind,
   },
   {
     name: "Sass",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio vitae architecto quo officia magnam. Cupiditate, harum. Culpa nihil deserunt facilis, corrupti cumque eligendi beatae!",
+      "I use Sass because it makes CSS much more powerful and maintainable. Features like variables, mixins, and loops allow me to write cleaner, reusable, and modular code. This makes managing styles in larger projects smoother. Sass also saves me time on large-scale projects while keeping the code well-organized.",
     image: sass,
   },
   {
     name: "Typescript",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio vitae architecto quo officia magnam. Cupiditate, harum. Culpa nihil deserunt facilis, corrupti cumque eligendi beatae!",
+      "TypeScript is one of my favorite tools because it helps me write more robust and reliable code. By adding static types to JavaScript, TypeScript enables me to catch errors early in the development phase, reducing bugs in production. I value the improved readability and maintainability, especially for larger projects where type management becomes essential. TypeScript also enhances team collaboration by providing implicit documentation through types.",
     image: typescript,
   },
 ];
